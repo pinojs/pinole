999bc88180c66d9fd9c765e6c928c787616da00c
David Mark Clements <david.mark.clements@gmail.com>
2020-12-17 01:42:45 +0100

    experimments

--- test/test.js
+++ test/test.js
@@ -1,18 +1,25 @@
 import pinole from '../index.js'
+const transport = pinole({ 
+  transport: './fixtures/basic-transport.js', 
+  dest: '/Users/davidclements/code/pinole/out'
+})
+console.time('logging time')
+console.time('main thread time')
+let i = 4000
+while (i-- > 0) {
+// setTimeout(() => {
+  transport.write('{"level":30,"time":1531171074631,"msg":"hello world","pid":657,"hostname":"Davids-MBP-3.fritz.box"}\n')
+// }, 499)
+// setTimeout(() => {
+  transport.write('{"level":30,"time":1531171082399,"msg":"hello child!","pid":657,"hostname":"Davids-MBP-3.fritz.box","a":"property"}\n')
+// }, 500)
+}
+console.timeEnd('main thread time')
 
-const transport = pinole({ transport: 'pino-colada' })
 
-transport.write('{"level":30,"time":1531171074631,"msg":"hello world","pid":657,"hostname":"Davids-MBP-3.fritz.box"}\n')
-transport.write('{"level":30,"time":1531171082399,"msg":"hello child!","pid":657,"hostname":"Davids-MBP-3.fritz.box","a":"property"}\n')
 
-setTimeout(() => {
-  transport.data[0] = 'h'.charCodeAt(0)
-  transport.data[1] = 'i'.charCodeAt(0)
-  transport.data[2] = '\n'.charCodeAt(0)
-  transport.pos = 3
-  transport.meta[2] = 3
-}, 500)
-
-process.once('exit', () => {
+process.on('exit', () => {
   transport.flushSync()
+  console.timeEnd('logging time')
+  
 })
